5421c8303840c8ca3609f760b19b8aede131f0b2
johann <johannfazakas@gmail.com>
2020-09-05 13:44:31 +0300

    mvn spring application initialized with 2 mock endpoints

---
+++ src/main/java/ro/johann/thoughts/ThoughtsSpringApiApplication.java
@@ -0,0 +1,13 @@
+package ro.johann.thoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtsSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtsSpringApiApplication.class, args);
+	}
+
+}
