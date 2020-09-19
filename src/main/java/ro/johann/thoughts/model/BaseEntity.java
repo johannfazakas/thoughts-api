f1c587769778bddbfee6382728deea0b17aa6f29
johannfazakas <johannfazakas@gmail.com>
2020-09-19 11:20:31 +0300

    added callback methods on base entity to set creation and last modification date times

--- src/main/java/ro/johann/thoughts/model/BaseEntity.java
+++ src/main/java/ro/johann/thoughts/model/BaseEntity.java
@@ -4,12 +4,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
@@ -17,6 +15,7 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @EqualsAndHashCode
+@Slf4j
 public abstract class BaseEntity {
 
     @Id
@@ -26,4 +25,45 @@ public abstract class BaseEntity {
     private String id;
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+    @Column(name = "last_modified_on")
+    private LocalDateTime lastModifiedOn;
+
+    @PrePersist
+    protected void prePersist() {
+        log.info("prePersist >> {}", this);
+        var now = LocalDateTime.now();
+        createdAt = now;
+        lastModifiedOn = now;
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        log.info("preUpdate >> {}", this);
+        lastModifiedOn = LocalDateTime.now();
+    }
+
+    @PreRemove
+    protected void preRemove() {
+        log.info("preRemove >> {}", this);
+    }
+
+    @PostPersist
+    protected void postPersist() {
+        log.info("postPersist >> {}", this);
+    }
+
+    @PostUpdate
+    protected void postUpdate() {
+        log.info("postUpdate >> {}", this);
+    }
+
+    @PostRemove
+    protected void postRemove() {
+        log.info("postRemove >> {}", this);
+    }
+
+    @PostLoad
+    protected void postLoad() {
+        log.info("postLoad >> {}", this);
+    }
 }
